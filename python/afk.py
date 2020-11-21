0d3d8fc7235a0db19c57a1689b0b9b2b21df4d03
iceE-1591 <icee1591@gmail.com>
2020-11-21 17:39:18 +0900

    [#8] コマンドで寝落ちしたユーザーを飛ばすBotを作成しました

--- python/afk.py
+++ python/afk.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import discord
 import asyncio
 
-TOKEN = 'NzcyMzczMDQ5MzYxMDM5NDEy.X55udA.dAfCLYnW_AmvSDkGfrZbeEC6peY'
+TOKEN = ''
 bot = commands.Bot(command_prefix="¥",help_command=None)
 
 @bot.event
@@ -22,40 +22,39 @@ async def on_voice_state_update(member, before, after):
 
 @bot.command()
 async def mmv(ctx, mention):
-    server = bot.get_guild(770973096215707648)
+    server = bot.get_guild(603582455756095488)
     status = ctx.author.voice
     member_id = mention[3:21]
-    member = server.get_member(int(member_id))
-    if status is not None and member.voice is not None:
-        channel = status.channel
-        mention_channel = member.voice.channel
-        if channel.id == mention_channel.id:
-            if channel.id == 683864874539024397 or channel.id == 603582455756095492 or channel.id == 685652747076632623 or channel.id == 777803355098710046:
-                try:
-                    member = server.get_member(int(member_id))
-                except:
-                    await ctx.send("`¥mmv {Mention}`と入力してください。")
-                else:
-                    if member.voice.self_mute:
-                        await ctx.send("対象のユーザーがミュートなので実行されませんでした。")
-                        return
-                    else:
-                        afk_channel = ctx.guild.get_channel(770973905312808991)
-                        await member.move_to(afk_channel)
-                        await member.edit(mute=True)
-                        #channel = server.get_channel(778975668934672404)
-                        #await channel.send(f"{mention} 寝落ちされていたようなので、ミュート＆移動しました。")
+    try:
+        member = server.get_member(int(member_id))
+    except:
+        await ctx.send("`¥mmv {Mention}`と入力してください。")
     else:
-        await ctx.send("vcに参加してください。")
+        if status is not None and member.voice is not None:
+            channel = status.channel
+            mention_channel = member.voice.channel
+            if channel.id == mention_channel.id:
+                if channel.id == 683864874539024397 or channel.id == 603582455756095492 or channel.id == 685652747076632623 or "２人用" in channel.name:
+                        if member.voice.self_mute:
+                            await ctx.send("対象のユーザーがミュートなので実行されませんでした。")
+                            return
+                        else:
+                            afk_channel = server.get_channel(778975759669788702)
+                            await member.move_to(afk_channel)
+                            await member.edit(mute=True)
+                            for channel in server.text_channels:
+                                if channel.topic == str(member_id):
+                                    await channel.send(f"{mention} 寝落ちされていたようなので、ミュート＆移動しました。")
+                                    break
+                            else:
+                                await ctx.send('チャンネルが見つかりませんでした。')
+            else:
+                await ctx.send("同じvcチャンネルに参加してください。")
+        else:
+            await ctx.send("vcに参加してください。")
 
 @bot.command()
 async def m(ctx):
     await ctx.author.edit(mute=False)
-
-@bot.command()
-async def test(ctx, mention):
-    member_id = mention[3:21]
-    member = ctx.guild.get_member(int(member_id))
-    await ctx.send(member.voice)
 
 bot.run(TOKEN)
