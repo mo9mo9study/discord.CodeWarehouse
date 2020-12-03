cfd05323ca82fb0fb29b3d9480bdeec9a70815eb
iceE-1591 <icee1591@gmail.com>
2020-12-03 21:37:02 +0900

    [#27] TOKENを削除しました

--- python/management.py
+++ python/management.py
@@ -3,7 +3,7 @@ import discord
 import asyncio
 
 intents = discord.Intents.all()
-TOKEN = 'NjUwMDE1MzE5NDg1NTc5Mjc1.XfZX-w.eNAaAKWjHsF8-qHDqR7DiLkshrg' #TOKENを入力してください
+TOKEN = ''  #TOKENを入力してください
 prefix = "/" #お好きなプレフィックス
 
 bot = commands.Bot(command_prefix=prefix,help_command=None,intents=intents)
