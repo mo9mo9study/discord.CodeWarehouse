50dab9b67443a83d2814ec8a94ea0980fd8118db
iceE-1591 <icee1591@gmail.com>
2020-12-03 13:31:18 +0900

    [#21] vcに入った時にroleを付与/はく奪するコードをjsからpyに移行させました

---
+++ python/management.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+import discord
+import asyncio
+
+intents = discord.Intents.all()
+TOKEN = '' #TOKENを入力してください
+prefix = "/" #お好きなプレフィックス
+
+bot = commands.Bot(command_prefix=prefix,help_command=None,intents=intents)
+
+bot.load_extension("Cogs.default")
+bot.load_extension("Cogs.Managements.voiceChannelJoinLeave_roleModify")
+
+bot.run(TOKEN)
