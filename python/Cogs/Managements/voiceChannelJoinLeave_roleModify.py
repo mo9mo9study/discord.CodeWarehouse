583155fa9aeb6c8b418215e24f2126252e3b694c
su-pleiades <su.impreza.itengineer@gmail.com>
2020-12-04 00:13:21 +0900

    [#26]本番で動作確認済み、小さい修正含む

--- python/Cogs/Managements/voiceChannelJoinLeave_roleModify.py
+++ python/Cogs/Managements/voiceChannelJoinLeave_roleModify.py
@@ -4,6 +4,18 @@ from discord.ext import commands
 import discord
 import asyncio
 
+
+# 「作業部屋用チャット」表示権限ID
+workRoomChatRoleId = 763401369784156211
+# 「ラウンジ用チャット」表示権限ID
+loungeChatRoleId = 763400537257148446
+# 「musicbot操作用」表示権限ID
+musicChatRokeId = 710333297598922752
+# 「作業部屋」ID
+workRoomVoiceChatId = 683864874539024397
+# 「ラウンジ」ID
+loungeVoiceChatId = 603582455756095492
+
 class VoiceJoin_Role(commands.Cog):
 
     def __init__(self,bot):
@@ -11,21 +23,37 @@ class VoiceJoin_Role(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self,member, before, after):
-        if after.channel is not None:                         #vcに参加した時
-            if after.channel.id == A:                         #変数Aには作業部屋チャンネルのidを入れて下さい
-                await self.AddRole(member, a, b)              #引数a,bにはmusic_role,作業部屋用チャットRoleのidを入れてください
-            elif after.channel.id == B:                       #変数Bにはラウンジチャンネルのidを入れて下さい
-                await AddRole(member, a, c)                   #引数a,cにはmusic_role,ラウンジ用チャットRoleのidを入れてください
-        if before.channel is not None:
-            if before.channel.id in (A,B):                    #変数A,Bには作業部屋チャンネル,ラウンジチャンネルのidを書いてください
-                await self.RemoveRole(member, a, b, c)        #引数a,b,cにはmusic_role,作業部屋用チャットRole,ラウンジ用チャットRoleのidを書いてください
+        print(0)
+        print("after: ", after)
+        print("before: ", before)
+        if after.channel != before.channel: 
+            print("-------------------")
+            print(member.name)
+            print(1)
+            if after.channel is not None:                         #vc参加時
+                print(2)
+                if after.channel.id == workRoomVoiceChatId:
+                    print("after: ",after.channel.name)
+                    print(3)
+                    await self.AddRole(member, musicChatRokeId, workRoomChatRoleId)
+                elif after.channel.id == loungeVoiceChatId:
+                    print(4)
+                    await self.AddRole(member, musicChatRokeId, loungeChatRoleId)
+            if before.channel is not None:                        #vc退席時
+                print("before: ",before.channel.name)
+                print(5)
+                if before.channel.id in (workRoomVoiceChatId, loungeVoiceChatId):
+                    await self.RemoveRole(member, musicChatRokeId, workRoomChatRoleId, loungeChatRoleId)
 
     async def AddRole(self,member,*args):
+        print(6)
         for role_id in args:
             role = member.guild.get_role(role_id)
+            print(role.name)
             await member.add_roles(role)
 
     async def RemoveRole(self,member,*args):
+        print(7)
         for role_id in args:
             role = member.guild.get_role(role_id)
             await member.remove_roles(role)
--- python/management.py
+++ python/management.py
@@ -2,13 +2,15 @@ from discord.ext import commands
 import discord
 import asyncio
 
+import setting
+
 intents = discord.Intents.all()
-TOKEN = ''  #TOKENを入力してください
-prefix = "/" #お好きなプレフィックス
+TOKEN = setting.dToken
+prefix = "¥"
 
 bot = commands.Bot(command_prefix=prefix,help_command=None,intents=intents)
 
 bot.load_extension("Cogs.default")
-bot.load_extension("voiceChannelJoinLeave_roleModify")
+bot.load_extension("Cogs.Managements.voiceChannelJoinLeave_roleModify")
 
 bot.run(TOKEN)
