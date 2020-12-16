806691156a50f047f6409a59248e2b58b3c10289
iceE-1591 <icee1591@gmail.com>
2020-12-17 06:05:29 +0900

    個人で使えるピン止め機能をCogに対応させました。

--- python/management.py
+++ python/management.py
@@ -13,5 +13,6 @@ bot = commands.Bot(command_prefix=prefix,help_command=None,intents=intents)
 bot.load_extension("Cogs.default")
 #bot.load_extension("Cogs.Managements.voiceChannelJoinLeave_roleModify")
 bot.load_extension("Cogs.Managements.rolesmanager")
+bot.load_extension("Cogs.Managements.PersonalPin")
 
 bot.run(TOKEN)
