3e6fa87e3838e177eefda2984e841e8e96035784
iceE-1591 <icee1591@gmail.com>
2021-01-31 20:57:03 +0900

    [#79]分報カテゴリーが50を超えたときの処理を追加

--- python/Cogs/Managements/times.py
+++ python/Cogs/Managements/times.py
@@ -14,7 +14,9 @@ class Times(commands.Cog):
         self.ROLE_ID = 801060326627999774  # announceチャンネルでメンションする役職のid
         self.ACTIVE_CATEGORY_ID = 709805664163332147  # activetimesカテゴリーのid
         self.az09_Channel_ID = 673004651871993866  # A-Z:数字の分報カテゴリーid
+        self.az09_Channel_ID2 = 805274560705724436  # A-Z:数字の分報カテゴリーid(2つ目)
         self.OTHER_CHANNEL_ID = 719095356218146879  # その他の分報カテゴリーid
+        self.OTHER_CHANNEL_ID2 = 805275220923121714  # その他の分報カテゴリーid(2つ目)
         self.EMOJIS = ["1⃣","2⃣","3⃣","4⃣","5⃣","6⃣"] #Tutorialメッセージに追加するリアクション一覧
         self.loop.start()
 
@@ -25,14 +27,13 @@ class Times(commands.Cog):
         self.ANNOUNCE = self.GUILD.get_channel(self.ANNOUNCE_ID)
         self.ACTIVE_CATEGORY = self.GUILD.get_channel(self.ACTIVE_CATEGORY_ID)
         self.az09_Channel = self.GUILD.get_channel(self.az09_Channel_ID)
+        self.az09_Channel2 = self.GUILD.get_channel(self.az09_Channel_ID2)
         self.OTHER_CHANNEL = self.GUILD.get_channel(self.OTHER_CHANNEL_ID)
+        self.OTHER_CHANNEL2 = self.GUILD.get_channel(self.OTHER_CHANNEL_ID2)
         self.ROLE = self.GUILD.get_role(self.ROLE_ID)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # BOTへのメッセージ送信時にもイベントが走るので、BOTのDM上のイベントはreturnする
-        if isinstance(message.channel, discord.DMChannel):
-            return
         if message.channel.category_id == self.ACTIVE_CATEGORY_ID:
             return
         # ---------------times作成処理---------------
@@ -42,7 +43,7 @@ class Times(commands.Cog):
                 user_id = int(user_id)
                 for channel in self.GUILD.text_channels:
                     if channel.topic == str(user_id):
-                        print(f"(userid: {str(user_id)})timesチャンネルが既に存在するので、作成しませんでした。")
+                        print("timesチャンネルが既に存在するので、作成しませんでした。")
                         break
                 else:
                     await self.channelCreateSend(self.getMember(user_id))
@@ -107,9 +108,9 @@ class Times(commands.Cog):
     #announce用のembedを作成
     def createAnnounce(self):
         az09, other = self.getChannelTotalNumber()
-        embed = discord.Embed(title=f"times_Channel総数： {az09+other}個")
-        embed.add_field(name=f"{self.az09_Channel.name}の総数：", value=f"{az09}個", inline=True)
-        embed.add_field(name=f"{self.OTHER_CHANNEL.name}の総数：", value=f"{other}個", inline=True)
+        embed = discord.Embed(title=f"times_Channel総数:{az09+other}ch")
+        embed.add_field(name=f"{self.az09_Channel.name}のtimes総数", value=f"{az09}ch", inline=False)
+        embed.add_field(name=f"{self.OTHER_CHANNEL.name}のtimes総数", value=f"{other}ch", inline=False)
         return embed
 
     #---createAnnounceメソッドからのみ呼び出される---
@@ -130,29 +131,30 @@ class Times(commands.Cog):
         activeChannels = self.ACTIVE_CATEGORY.text_channels
         return activeChannels
 
+    def getChannelCount(self, which):
+        if which == "az":
+            return len(self.az09_Channel.text_channels)
+        elif which == "other":
+            return len(self.OTHER_CHANNEL.text_channels)
+
     # ---------------定期処理---------------
     #午前2:00に実行されます
-    @tasks.loop(seconds=60)
+    @tasks.loop(seconds=59)
     async def loop(self):
         await self.bot.wait_until_ready()
         now = datetime.now().strftime('%H:%M')
         if now == "02:00":
             for channel in self.getActiveChannels():
                 if channel.name[6].encode('utf-8').isalnum():
-                    await channel.edit(category=self.az09_Channel)
+                    if self.getChannelCount("az") == 50:
+                        await channel.edit(category=self.az09_Channel2)
+                    else:
+                        await channel.edit(category=self.az09_Channel)
                 else:
-                    await channel.edit(category=self.OTHER_CHANNEL)
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def times_reset(self, ctx):
-        for channel in self.getActiveChannels():
-            if channel.name[6].encode('utf-8').isalnum():
-                print(channel.name[6])
-                await channel.edit(category=self.az09_Channel)
-            else:
-                await channel.edit(category=self.OTHER_CHANNEL)    
-
+                    if self.getChannelCount("other") == 50:
+                        await channel.edit(category=self.OTHER_CHANNEL2)
+                    else:
+                        await channel.edit(category=self.OTHER_CHANNEL)
 
 def setup(bot):
     return bot.add_cog(Times(bot))
