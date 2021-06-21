d46fa96c9ec0efb61a12d5d0ecbcdcc01a479e78
su-pleiades <su.impreza.itengineer@gmail.com>
2021-06-22 00:22:53 +0900

    [#111]メンバー参加時にはメンバー情報追加、脱退時には削除するようにしました

--- Cogs/Managements/joinLeaveLog.py
+++ Cogs/Managements/joinLeaveLog.py
@@ -2,6 +2,12 @@ from discord.ext import commands
 import discord
 import asyncio
 import inspect
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
+import emojis
+import re
+
+from mo9mo9db.dbtables import Studymembers
+from mo9mo9db.dbsession import get_db_engine
 
 class Join_Leave_Log(commands.Cog):
 
@@ -10,6 +16,10 @@ class Join_Leave_Log(commands.Cog):
         self.GUILD_ID = 603582455756095488 # mo9mo9サーバーID
         self.CHANNEL_ID = 708303469882114058 #参加・離脱ログ用チャンネルid
         self.old_invite_list = []
+
+    def remove_emoji(self, src_str):
+        decode_str = emojis.decode(src_str)
+        return re.sub(":.*:", "" ,decode_str)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -33,12 +43,27 @@ class Join_Leave_Log(commands.Cog):
         new_invite_list_uses = list(map(lambda invite: invite.uses, new_invite_list))
         inviter_mention = await self.uses_if(new_invite_list_uses,old_invite_list_uses,new_invite_list)
         text = f"**{member.name} (id:__{str(member.id)}__) が参加しました。【 招待者：{inviter_mention} 】**"
+        # dbにメンバー情報書き込み
+        session = Studymembers.session()
+        add_date = Studymembers(
+            guild_id = member.guild.id,
+            member_id = member.id,
+            member_name = self.remove_emoji(member.display_name),
+            joined_dt = member.joined_at,
+            enrollment = True
+        )
+        Studymembers.insert(add_date)
         print(text)
         await self.CHANNEL.send(text)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         text = f"**{member.name} (id:__{str(member.id)}__) が離脱しました。**"
+        # dbからメンバー情報を削除
+        session = Studymembers.session()
+        delete_date = session.query(Studymembers).filter(Studymembers.member_id==member.id).first()
+        session.delete(delete_date)
+        session.commit()
         print(text)
         await self.CHANNEL.send(text)
 
