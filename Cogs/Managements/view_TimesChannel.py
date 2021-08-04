ab137e38504fd0b9e3f6081c9fab5ad71ad2b2a4
su-pleiades <su.impreza.itengineer@gmail.com>
2021-08-04 09:08:03 +0900

    timesが見つからない時でも押したリアクションが削除される処理を追加

--- Cogs/Managements/view_TimesChannel.py
+++ Cogs/Managements/view_TimesChannel.py
@@ -2,28 +2,31 @@ from discord.ext import commands
 import discord
 import asyncio
 
+
 class ViewTimesChannel(commands.Cog):
 
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
         self.guild_id = 603582455756095488
-        self.channel_id = 792369191843135488 #下のメッセージがあるchannelのid
+        self.channel_id = 792369191843135488  # 下のメッセージがあるchannelのid
         self.second = 5
 
     # 移動用のembedメッセージ
     @commands.Cog.listener()
     async def on_ready(self):
-        self.channel = self.bot.get_guild(self.guild_id).get_channel(self.channel_id)
+        self.channel = self.bot.get_guild(
+            self.guild_id).get_channel(self.channel_id)
         await self.channel.purge()
 
-        embed = discord.Embed(title="各自timesへの移動を簡単にします", description="- あなたのtimesへのリンク(移動手段)を5秒表示します")
+        embed = discord.Embed(title="各自timesへの移動を簡単にします",
+                              description="- あなたのtimesへのリンク(移動手段)を5秒表示します")
         embed.add_field(name=" 👇 使い方", value="（超簡単）このメッセージにリアクションをするだけ‼️ ")
         self.message = await self.channel.send(embed=embed)
         self.message_id = self.message.id
         await self.message.add_reaction("🛎️")
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self,payload):
+    async def on_raw_reaction_add(self, payload):
         if payload.member.bot:
             return
         if payload.message_id == self.message_id:
@@ -34,15 +37,18 @@ class ViewTimesChannel(commands.Cog):
                 if times_channel.topic == str(member_id):
                     msg = await self.channel.send(times_channel.mention)
                     await self.time_sleep(msg)
-                    await select_msg.remove_reaction(payload.emoji, payload.member)
+                    await select_msg.remove_reaction(payload.emoji,
+                                                     payload.member)
                     break
             else:
                 msg = await self.channel.send("timesチャンネルが見つかりませんでした。")
                 await self.time_sleep(msg)
+                await select_msg.remove_reaction(payload.emoji, payload.member)
 
-    async def time_sleep(self,msg):
+    async def time_sleep(self, msg):
         await asyncio.sleep(self.second)
         await msg.delete()
+
 
 def setup(bot):
     return bot.add_cog(ViewTimesChannel(bot))
