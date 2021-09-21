79b384dda171e7d0839e22fb9dbea7238ae5c3be
su-pleiades <su.impreza.itengineer@gmail.com>
2021-09-21 15:19:06 +0900

    [#128] rolemanagerから呼び出される際に正常に動作するよう修正
    - このクラス内の処理であれば問題なく処理が終わるが、
    別のファイルrolemanager.pyから呼ばれる際に必要なギルド情報が
    流れてこないため、こちら側でギルドをfetchして補完するようにしました

--- Cogs/Managements/voiceChannelJoinLeave_roleModify.py
+++ Cogs/Managements/voiceChannelJoinLeave_roleModify.py
@@ -7,11 +7,16 @@ class VoiceJoin_Role(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.GUILD_ID = 603582455756095488
         self.workRoomChatRoleId = 763401369784156211  # 「作業部屋用チャット」表示権限ID
         self.loungeChatRoleId = 763400537257148446  # 「ラウンジ用チャット」表示権限ID
         self.musicChatRokeId = 710333297598922752  # 「musicbot操作用」表示権限ID
         self.workRoomVoiceChatId = 683864874539024397  # 「作業部屋」ID
         self.loungeVoiceChatId = 603582455756095492  # 「ラウンジ」ID
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.GUILD = self.bot.get_guild(self.GUILD_ID)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -38,13 +43,13 @@ class VoiceJoin_Role(commands.Cog):
 
     async def AddRole(self, member, *args):
         for role_id in args:
-            role = member.guild.get_role(role_id)
+            role = self.GUILD.get_role(role_id)
             print(f"[INFO] {member.name}に権限「{role.name}」を付与")
             await member.add_roles(role)
 
     async def RemoveRole(self, member, *args):
         for role_id in args:
-            role = member.guild.get_role(role_id)
+            role = self.GUILD.get_role(role_id)
             print(f"[INFO] {member.name}から権限「{role.name}」を剥奪")
             await member.remove_roles(role)
 
