f29a130bac872996f3ee2c90e3c755ab84242bb0
su-pleiades <su.impreza.itengineer@gmail.com>
2021-09-26 22:32:34 +0900

    [#131] 初回手動実行用の処理を追加
    - 手動時と定期実行時の２つのトリガーで同じ処理が流れるようメインの処理を関数化
    - チャンネル削除の際にDiscord監査ログの理由を追加

--- Cogs/Managements/regularly_times_delete.py
+++ Cogs/Managements/regularly_times_delete.py
@@ -9,15 +9,34 @@ class RegularlyTimesDelete(commands.Cog):
 
     def self(self, bot):
         self.bot = bot
-        self.GUILD_ID = 0
+        self.GUILD_ID = 603582455756095488
         # botの実行ログをdiscordに送信する場合
-        self.LOG_CHANNEL_ID = 0
+        self.LOG_CHANNEL_ID = 801060150433153054
 
     async def on_ready(self):
         self.GUILD = self.bot.get_guild(self.GUILD_ID)
         self.LOG_CHANNEL = self.bot.get_guild(self.LOG_CHANNEL_ID)
 
+    async def times_delete(self):
+        deletetimes_count = 0
+        dt_retentionperiod = datetime.now() \
+            - relativedelta(months=self.RETENTIONPERIOD)
+        print(len(self.GUILD.text_channels))
+        for times_channel in self.GUILD.text_channels:
+            if times_channel.name[0:6] == "times_":
+                times_lastmessage = await times_channel.history(limit=1).flatten()  # noqa: E501
+                last_message = await times_channel.fetch_message(times_lastmessage[0].id)  # noqa: E501
+                if dt_retentionperiod > last_message.created_at:
+                    reason_msg = f"最終投稿日: {last_message.created_at}"
+                    print(f"[DEBUG] ( {times_channel.name} ) {reason_msg}")
+                    # 以下の処理がこのコードでメインとなる処理になる
+                    # await times_channel.delete(reason=f"３ヶ月以上未使用のため({reason_msg})")  # noqa: E501
+                    deletetimes_count += 1
+        # botの実行ログをdiscordに送信する場合
+        return deletetimes_count
+
     # ちょっとおまけで作ったコマンド
+
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def admin_timesinfo(self, ctx):
@@ -26,6 +45,16 @@ class RegularlyTimesDelete(commands.Cog):
         embed = discord.Embed(title="現在のtimes保持期間は、",
                               description=f"{self.RETENTIONPERIOD}ヶ月 (最終投稿日:{dt_retentionperiod}以前削除)")  # noqa: E501
         await ctx.reply(embed=embed)
+
+    # 手動実行用
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def admin_timesdelete(self, ctx):
+        deletetimes_count = self.times_delete()
+        # ログの送信
+        send_msg = f"[INFO] delete(schedule)times: {deletetimes_count}"
+        await self.LOG_CHANNEL.channel.send(send_msg)
 
     # 毎時定期実行するトリガーで、深夜3時に実行される
     # 設定された保持期間を過ぎて使用されていないtimesチャンネルを削除する処理
@@ -36,20 +65,10 @@ class RegularlyTimesDelete(commands.Cog):
         if date.today().weekday() != 0:  # 週一回の実行予定
             return
         if datetime.now().strftime('%H') == "03":
-            count = 0
-            dt_retentionperiod = datetime.now() \
-                - relativedelta(months=self.RETENTIONPERIOD)
-            print(len(self.GUILD.text_channels))
-            for times_channel in self.GUILD.text_channels:
-                if times_channel.name[0:6] == "times_":
-                    times_lastmessage = await times_channel.history(limit=1).flatten()  # noqa: E501
-                    last_message = await times_channel.fetch_message(times_lastmessage[0].id)  # noqa: E501
-                    if dt_retentionperiod > last_message.created_at:
-                        count += 1
-                        print(f"[DEBUG] ( {times_channel.name} ) 最終投稿日: {last_message.created_at}")  # noqa: E501
-            # send_msg = f"[INFO] done, delete(schedule)times: {count}"
-            # botの実行ログをdiscordに送信する場合
-            # await self.LOG_CHANNEL.channel.send(send_msg)
+            deletetimes_count = self.times_delete()
+            # ログの送信
+            send_msg = f"[INFO] delete(schedule)times: {deletetimes_count}"
+            await self.LOG_CHANNEL.channel.send(send_msg)
 
 
 def setup(bot):
