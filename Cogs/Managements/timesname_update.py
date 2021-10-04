d2f35d07f2023a07eabfebda9ddc403f62d9a200
su-pleiades <su.impreza.itengineer@gmail.com>
2021-10-04 23:15:03 +0900

    [#132] 動作想定通りなことを確認済み
    - 変数名の小文字を一部大文字に変更
    - 関数名のミスを修正
    - チャンネル内の文字一括削除メソッドの活性化
    - ログメッセージの送信処理追加

--- Cogs/Managements/timesname_update.py
+++ Cogs/Managements/timesname_update.py
@@ -4,20 +4,22 @@ import discord
 
 class TimesnameUpdate(commands.Cog):
 
-    def self(self, bot):
+    def __init__(self, bot):
         self.bot = bot
         self.GUILD_ID = 603582455756095488
         self.CHANNEL_ID = 894577054262116426
+        self.LOG_CHANNEL_ID = 801060150433153054
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.guild = self.bot.get_guild(self.GUILD_ID)
-        self.channel = self.guild.get_channel(self.CHANNEL_ID)
-        # await self.channel.purge()
+        self.GUILD = self.bot.get_guild(self.GUILD_ID)
+        self.CHANNEL = self.GUILD.get_channel(self.CHANNEL_ID)
+        self.LOG_CHANNEL = self.GUILD.get_channel(self.LOG_CHANNEL_ID)
+        await self.CHANNEL.purge()
         embed = discord.Embed(title="あなたのtimesのチャンネル名を更新します",
                               description="- discord名(ニックネーム)を変更した人などは活用してみてね")
         embed.add_field(name=" 👇 使い方", value="（超簡単）このメッセージにリアクションをするだけ‼️ ")
-        self.message = await self.channel.send(embed=embed)
+        self.message = await self.CHANNEL.send(embed=embed)
         self.message_id = self.message.id
         await self.message.add_reaction("🛎️")
 
@@ -27,13 +29,12 @@ class TimesnameUpdate(commands.Cog):
         # reaction_remove同様にreaction_addでもfetch_memberでmenberオブジェクトを取得する
         # discord.Userではnickの属性がないのでdiscord.Memberを取得する
         # discord.Member.display_nameはニックネームNoneならアカウント名が適用される
-        self.GUILD = self.bot.get_guild(self.GUILD_ID)
         member = await self.GUILD.fetch_member(payload.user_id)
         if member.bot:
             return
         if payload.message_id == self.message_id:
             member_id = payload.member.id
-            select_msg = await self.channel.fetch_message(payload.message_id)
+            select_msg = await self.CHANNEL.fetch_message(payload.message_id)
             for times_channel in self.GUILD.text_channels:
                 if times_channel.topic == str(member_id):
                     before_name = times_channel.name
@@ -49,6 +50,8 @@ class TimesnameUpdate(commands.Cog):
                         embed.add_field(name="変更後のチャンネル名",
                                         value=after_name)
                         await times_channel.send(embed=embed)
+                        log_msg = f"[INFO] チャンネル名変更({before_name} ▶ {after_name}"  # noqa #501
+                        await self.LOG_CHANNEL.send(log_msg)
                     else:
                         embed = discord.Embed(title="変更前と変更後の名前に差分がありませんでした",
                                               description=f"{member.mention} チャンネル名を変更しませんでした")  # noqa #501
