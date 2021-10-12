653047178c50bf80863d0bbd63c6190fa7659082
su-pleiades <su.impreza.itengineer@gmail.com>
2021-10-12 22:01:32 +0900

    [#97] 必要な値を取得できる様に正規表現を修正
    - 勉強記録のログの出力メッセージ変更に合わせて修正
    - 不要なimport文の削除
    - ログメッセージ追加
    - 一部変数の変更

--- Cogs/Managements/slotstudyrecord.py
+++ Cogs/Managements/slotstudyrecord.py
@@ -2,26 +2,26 @@ from datetime import datetime, timedelta
 import random
 import re
 
-import discord
 from discord.ext import commands
-import asyncio
+
 
 class SlotStudyrecord(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.GUILD_ID = 603582455756095488 # mo9mo9サーバーID
-        self.RECORD_CHANNEL_ID = 618081921611726851 # 勉強記録チャンネル
-        self.CHANNEL_ID = 684736099565830169 # 勉強スロット当選者
-        self.notOutputNum = list(range(1,5))
-        self.slot1 = ['🙈','👹','👾']
-        self.slot2 = ['🙉','🤡','💀']
-        self.slot3 = ['🙊','💩','👻']
+        self.GUILD_ID = 603582455756095488  # mo9mo9サーバーID
+        self.RECORD_CHANNEL_ID = 618081921611726851  # 勉強記録チャンネル
+        self.CHANNEL_ID = 684736099565830169  # 勉強スロット当選者
+        self.LOG_CHANNEL_ID = 801060150433153054
+        self.notOutputTime = 20
+        self.slot1 = ['🙈', '👹', '👾']
+        self.slot2 = ['🙉', '🤡', '💀']
+        self.slot3 = ['🙊', '💩', '👻']
 
     def intRandom(self, emojis):
         i = random.randrange(len(emojis))
         return i
-    
+
     async def addReaction(self, message, emojis, i):
         reactionId = emojis[i]
         await message.add_reaction(reactionId)
@@ -29,21 +29,25 @@ class SlotStudyrecord(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.CHANNEL = self.bot.get_channel(self.CHANNEL_ID)
+        self.LOG_CHANNEL = self.bot.get_channel(self.LOG_CHANNEL_ID)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot: # BOTじゃなければreturn
+        if not message.author.bot:  # BOTじゃなければreturn
+            print(message)
             return
         # 勉強ログのメッセージのみtrue
         if message.channel.id != self.RECORD_CHANNEL_ID:
-            return 
+            return
         if 'Study time' in message.content:
-            result = re.compile(r'.*Study time  (.*) /分.*').match(message.content)
-            intStudyTime = int(result[1])
-            strName = re.compile(r'.*\[.*\] (.*) .*').match(message.content)
-            strName = strName[1]
+            result_studytime = re.compile(
+                r'.*Study time  (.*) /分.*').match(message.content)
+            intStudyTime = int(result_studytime[1])
+            result_name = re.compile(
+                r'.*\[.*\]\s*(.*)  Study time.*').match(message.content)
+            strName = result_name[1]
             # 5分以内の記録では動作しないようにするため
-            if intStudyTime in self.notOutputNum: 
+            if intStudyTime < self.notOutputTime:
                 return
             else:
                 slotresult1 = self.intRandom(self.slot1)
@@ -59,9 +63,10 @@ class SlotStudyrecord(commands.Cog):
                 guildMembers = message.guild.members
                 for member in guildMembers:
                     if member.name == strName:
-                        msg = f'{member.mention}\n[{now:%m/%d %H:%M} ] 勉強時間ボーナススロット大当たり！！{intStudyTime}分の勉強お疲れ様！'
-                        print(msg)
-                        await self.CHANNEL.send(msg)
+                        send_msg = f'{member.mention}\n[ {now:%m/%d %H:%M} ] 勉強時間ボーナススロット大当たり🎉 {intStudyTime}分の勉強お疲れ様 🍵'  # noqa #501
+                        log_msg = f"[INFO] {strName}が勉強スロットに当選"
+                        await self.CHANNEL.send(send_msg)
+                        await self.LOG_CHANNEL.send(log_msg)
                         break
 
 
